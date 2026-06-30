b539b8d3573247954c698932a5b01273692560d9
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-06-30 15:05:44 -0300

    Continue

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod cli;
+mod gui;
+
+use anyhow::Result;
+use clap::Parser as _;
+
+fn main() -> Result<()> {
+    let args = cli::Args::parse();
+    match args.mode.unwrap_or(cli::Mode::Gui) {
+        cli::Mode::Gui => gui::run().map_err(|err| anyhow::anyhow!("{err}")),
+        cli::Mode::Cli(args) => cli::run(args),
+    }
+}
